785763784392e8b3a5e6b54cd393bedf52a5ce4e
vranjesluka <vranjes.luka@gmail.com>
2017-03-04 18:01:48 +0100

    Solve the challenge "Arrays: Left Rotation" - 1st Data structures challenge in Cracking the Coding Interview course on HackerRank

---
+++ src/hackerrank/crackingthecodinginterview/C1_ArraysLeftRotation.java
@@ -0,0 +1,23 @@
+package hackerrank.crackingthecodinginterview;
+
+import java.util.Scanner;
+
+/**
+ * Created by vranjesluka on 04/03/2017.
+ */
+public class C1_ArraysLeftRotation {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int d = in.nextInt();
+        int a[] = new int[n];
+        for (int a_i = 0; a_i < n; a_i++) {
+            a[a_i] = in.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            int index = (d + i) % n;
+            System.out.print(a[index] + " ");
+        }
+    }
+}

1db0675ae76277bfb39ec58a7f6dc84bf97e3795
vranjesluka <vranjes.luka@gmail.com>
2017-03-05 18:36:54 +0100

    Solve the challenge "Hash Tables: Ransom Note" - 3rd Data structures challenge in Cracking the Coding Interview course on HackerRank

---
+++ src/hackerrank/crackingthecodinginterview/datastructures/C3_HashTablesRansomNote.java
@@ -0,0 +1,42 @@
+package hackerrank.crackingthecodinginterview.datastructures;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/**
+ * Created by vranjesluka on 05/03/2017.
+ */
+public class C3_HashTablesRansomNote {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n = in.nextInt();
+        HashMap<String, Integer> magazineWords = new HashMap<>(m);
+        for (int magazine_i = 0; magazine_i < m; magazine_i++) {
+            String currentWord = in.next();
+            if (magazineWords.containsKey(currentWord)) {
+                int currentNumber = magazineWords.get(currentWord) + 1;
+                magazineWords.put(currentWord, currentNumber);
+            } else {
+                magazineWords.put(currentWord, 1);
+            }
+        }
+        boolean canWrite = true;
+        for (int ransom_i = 0; ransom_i < n; ransom_i++) {
+            String currentWord = in.next();
+            if (!magazineWords.containsKey(currentWord) || magazineWords.get(currentWord) < 1) {
+                canWrite = false;
+                break;
+            } else {
+                int currentNumber = magazineWords.get(currentWord) - 1;
+                magazineWords.put(currentWord, currentNumber);
+            }
+        }
+        if (canWrite) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
